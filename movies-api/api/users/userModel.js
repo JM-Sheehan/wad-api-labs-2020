10b0f47a9d1e39b7229a333ad1e303ecec97f667
Jamie Sheehan <jamie.michael.sheehan@gmail.com>
2021-08-11 20:37:13 +0100

    Topic 10 - Salt and hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,11 +1,7 @@
+import bcrypt from 'bcrypt-nodejs';
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-
-// const UserSchema = new Schema({
-//     username: { type: String, unique: true, required: true },
-//     password: { type: String, required: true }
-// });
 
 const MovieSchema = new Schema({
     id: Number,
@@ -22,11 +18,32 @@ UserSchema.statics.findByUserName = function (username) {
     return this.findOne({ username: username });
 };
 
-UserSchema.methods.comparePassword = function (candidatePassword) {
-    const isMatch = this.password === candidatePassword;
-    if (!isMatch) {
-        throw new Error('Password mismatch');
-    }
-    return this;
+UserSchema.methods.comparePassword = function(passw, cb) {
+    bcrypt.compare(passw, this.password, (err, isMatch) => {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
 };
+
+UserSchema.pre('save', function(next) {
+    const user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, (err, salt)=> {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, null, (err, hash)=> {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
 export default mongoose.model('User', UserSchema);
